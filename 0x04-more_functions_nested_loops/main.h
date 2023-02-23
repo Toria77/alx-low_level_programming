f7899746a12adba0ab512b9eef471af1105a5b42
victoria <toriafortune@gmail.com>
2023-02-23 06:49:48 -0800

    task 4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -17,4 +21,3 @@ void print_number(int n);
 
 int numDigits(int n);
 #endif
-
